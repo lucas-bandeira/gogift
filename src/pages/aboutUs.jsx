39852cc48171dd90c06d2b55572cd716293d692f
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-05-17 17:27:45 -0300

    criado pagina sobre nos

---
+++ src/pages/aboutUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>
+                    About Us | Go Gift
+                </title>
+            </Head>
+            <div>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </>
+
+    )
+}
