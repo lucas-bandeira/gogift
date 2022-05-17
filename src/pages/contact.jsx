f319566e2a017c966d069196bb18a6a50021812b
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-05-17 17:27:25 -0300

    criado pagina de contato

---
+++ src/pages/contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Contact() {
+    return (
+        <>
+            <Head>
+                <title>
+                    Contact | Go Gift
+                </title>
+            </Head>
+            <div>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </>
+
+    )
+}
