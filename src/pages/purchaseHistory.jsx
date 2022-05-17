6b170ecad3ec1bf8daab2704d86d68e54e71f823
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-05-17 17:26:49 -0300

    criado pagina de historico de compras

---
+++ src/pages/purchaseHistory.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function PurchaseHistory() {
+    return (
+        <>
+            <Head>
+                <title>
+                    Purchase History | Go Gift
+                </title>
+            </Head>
+            <div>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </>
+
+    )
+}
