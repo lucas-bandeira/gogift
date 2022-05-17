e75301b9970182200c3f6bcbee8184037e93807b
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-05-17 17:27:32 -0300

    criado pagina de categoria

---
+++ src/pages/category.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Category() {
+    return (
+        <>
+            <Head>
+                <title>
+                    Category | Go Gift
+                </title>
+            </Head>
+            <div>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </>
+
+    )
+}
