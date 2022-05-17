0697f581d4ffec20bd024db6b5e8cc5e73c332b3
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-05-17 17:27:09 -0300

    criado pagina de produtos

---
+++ src/pages/products.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Products() {
+    return (
+        <>
+            <Head>
+                <title>
+                    Products | Go Gift
+                </title>
+            </Head>
+            <div>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </>
+
+    )
+}
