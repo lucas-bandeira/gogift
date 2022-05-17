a48e8d3156a52fc0e38a96b5b5470c08b9bd5e69
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-05-17 17:27:16 -0300

    criado pagina de parceiros

---
+++ src/pages/partners.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Partners() {
+    return (
+        <>
+            <Head>
+                <title>
+                    Partners | Go Gift
+                </title>
+            </Head>
+            <div>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </>
+
+    )
+}
